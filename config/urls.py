25c8c2e77056bb9c33583c059fed4025856921d6
Ildar <gif8686@gmail.com>
2025-12-22 00:13:31 +0500

    config/urls.py fixed

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('catalog.urls', namespace='catalog')),
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
     path('', include('catalog.urls', namespace='catalog')),
 ]
