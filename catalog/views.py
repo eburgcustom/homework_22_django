9f898ac22a783e98ab20faee1fa3266a14f62050
Ildar <gif8686@gmail.com>
2025-12-28 01:18:38 +0500

    urls.py and views.py updated

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from .models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {
+        'products': products,
+        'title': 'Главная - Каталог товаров'
+    }
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
@@ -21,3 +27,15 @@ def contacts(request):
         return redirect('catalog:contacts')
 
     return render(request, 'contacts.html')
+
+
+def product_detail(request, pk):
+    """
+    Отображает детальную информацию о товаре
+    """
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product,
+        'title': f'Товар: {product.name}'
+    }
+    return render(request, 'product_detail.html', context)
