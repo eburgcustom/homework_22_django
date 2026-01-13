1a16e95a08bc4cf3c164a7733b0f47d1da645c59
Ildar <gif8686@gmail.com>
2026-01-14 01:27:35 +0500

    stylization of forms for products

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,8 +8,29 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'description', 'image', 'category', 'price']
         widgets = {
-            'description': forms.Textarea(attrs={'rows': 4}),
+            'description': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'min': '0', 'step': '0.01'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Добавляем классы для всех полей
+        for field_name, field in self.fields.items():
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
+            # Добавляем placeholder'ы
+            if field_name == 'name':
+                field.widget.attrs['placeholder'] = 'Введите название товара'
+            elif field_name == 'description':
+                field.widget.attrs['placeholder'] = 'Введите описание товара'
+            elif field_name == 'price':
+                field.widget.attrs['placeholder'] = '0.00'
+            # Добавляем обязательные поля
+            if field.required:
+                field.widget.attrs['required'] = 'required'
 
     def clean_name(self):
         name = self.cleaned_data['name']
