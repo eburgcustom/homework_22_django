1dc5aa7b29826cbab087eeb57a6edf6ef616fefc
Ildar <gif8686@gmail.com>
2026-01-28 22:06:45 +0500

    add permissions catalog/models.py

--- catalog/models.py
+++ catalog/models.py
@@ -20,19 +20,34 @@ class Product(models.Model):
     """
     Модель для товаров
     """
+    class PublicationStatus(models.TextChoices):
+        DRAFT = 'draft', 'Черновик'
+        PUBLISHED = 'published', 'Опубликовано'
+        MODERATION = 'moderation', 'На модерации'
+        REJECTED = 'rejected', 'Отклонено'
+
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    publication_status = models.CharField(
+        max_length=20,
+        choices=PublicationStatus.choices,
+        default=PublicationStatus.DRAFT,
+        verbose_name='Статус публикации'
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
-
-    def __str__(self):
-        return f"{self.name} ({self.category.name}) - {self.price}"
 
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
         ordering = ['name', 'category']
+        permissions = [
+            ('can_unpublish_product', 'Может отменять публикацию продукта'),
+        ]
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name}) - {self.price}"
