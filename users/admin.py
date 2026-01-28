31dbbd8fc98806febb8bc649fc09fb5234404fe3
Ildar <gif8686@gmail.com>
2026-01-28 15:41:05 +0500

    adding a user model to the admin area

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'username')
