256012fcc5db5444044cf6ee36744c3b4377ac19
Ildar <gif8686@gmail.com>
2026-01-31 10:03:45 +0500

    add RadisCache

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
